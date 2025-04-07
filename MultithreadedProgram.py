a7e46c0399b144468fa10befa0b7591583dff7b5
Priya9096 <priyakhairate0000@gmail.com>
2025-04-07 18:17:52 +0530

    added

---
+++ MultithreadedProgram.py
@@ -0,0 +1,28 @@
+import time
+import threading
+
+def print_numbers():
+    for i in range(1,6):
+        print(i)
+        time.sleep(1)
+
+def print_characters():
+    for i in ['A','B','C','D','E']:
+        print(i)
+        time.sleep(1)
+
+thread1 = threading.Thread(target=print_numbers)
+thread2 = threading.Thread(target=print_characters)
+thread1.start()
+thread2.start()
+
+print(f"Is thread1 alive? {thread1.is_alive()}")
+
+thread1.join()
+thread2.join()
+
+print(f"Is thread1 alive? {thread1.is_alive()}")
+
+print('All threads executed')
+
+
